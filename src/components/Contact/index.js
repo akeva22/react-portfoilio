7f4e73b7520e7ac46d380a47e83c4bca1d95b85b
Akeva Melchor <akeva.melchor@expel.io>
2022-08-08 16:32:56 -0700

    add contact form, and avi

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,77 @@
-import React from "react";
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 
-function Contact() {
+   
+function ContactForm() {
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' })
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, name: e.target.value })
+
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value); 
+            console.log(isValid); 
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+              } else {
+                setErrorMessage('');
+              }
+        } else {
+            if (!e.target.value.length) {
+              setErrorMessage(`${e.target.name} is required.`);
+            } else {
+              setErrorMessage('');
+            }
+          }
+          if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+          }
+      }
+      
+      console.log(formState);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+      }
+      
+      console.log(formState);
+
     return (
-        <section className='my-5'>
-            <div>
-                <h1 id='#contact'>Contact</h1>
-                <p>Acceptance tests, also known as application tests, are one of a few types of automated testing at our disposal in Ember. We will learn about the other types later, but what makes acceptance tests unique is that they test our app from the user's perspective—they are an automated version of the "click around and see if it works" testing we did earlier, which is exactly what we need..</p>
-            </div>
+        <section> 
+             <h1>Contact me</h1>
+             <form id="contact-form" onSubmit={handleSubmit}>
+      <div>
+  <label htmlFor="name">Name:</label>
+  <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+</div>
+<div>
+  <label htmlFor="email">Email address:</label>
+ <input type="email" defaultValue={email} name="email" onChange={handleChange} />
+</div>
+<div>
+  <label htmlFor="message">Message:</label>
+  <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+  {errorMessage && (
+  <div>
+    <p className="error-text">{errorMessage}</p>
+  </div>
+)}
+</div>
+<button type="submit">Submit</button>
+      </form>
         </section>
     )
+
 }
 
-export default Contact; 
+
+
+
+
+export default ContactForm; 
--- src/components/Work/Index.js
+++ src/components/Work/Index.js
@@ -6,15 +6,7 @@ function Work() {
         <section className='my-5'>
             <div>
                 <h1 id='#work'>Work</h1>
-                <p>We accomplished a lot in the last few chapters! Let's recap—we started with a blank canvas, added a few pages of content, styled everything to look pretty, dropped in a picture of Tomster, added links between our pages and amazingly, everything worked together flawlessly!
-
-                    But do we really know that everything is actually working? Sure, we clicked around a bit to confirm that things look as expected. But do we feel confident that we checked every page after the most recent change that we made?
-
-                    After all, most of us have experienced (or heard horror stories about) making a Small Tweak™ in one area of the app that inadvertently broke everything else when we weren't looking.
-
-                    Maybe we can write a checklist somewhere of all the things to check after making changes to our site. But surely, this will get out of hand as we add more features to our app. It is also going to get old really quickly—repetitive tasks like that are best left to robots.
-
-                    Hmm, robots. That's an idea. What if we can write this checklist and just get the computer to check everything for us? I think we just invented the idea of automated testing! Okay, maybe we were not the first to come up with the concept, but we independently discovered it so we still deserve some credit.</p>
+            <p>Portfoilio Will go here, should be fun.</p>
             </div>
         </section>
     )
